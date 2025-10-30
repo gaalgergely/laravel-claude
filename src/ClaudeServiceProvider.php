4b60c5ac807632b5bc48244870d75e8ccad73c3c
Gaál Gergely <gaalgergely@yahoo.com>
2025-10-30 20:02:16 +0100

    Update ClaudeServiceProvider.php

--- src/ClaudeServiceProvider.php
+++ src/ClaudeServiceProvider.php
@@ -5,20 +5,14 @@ namespace GergelyGaal\LaravelClaude;
 use GergelyGaal\LaravelClaude\Clients\HttpClaudeClient;
 use GergelyGaal\LaravelClaude\Contracts\ClaudeClientContract;
 use GergelyGaal\LaravelClaude\Services\Claude as ClaudeService;
-use Spatie\LaravelPackageTools\Package;
-use Spatie\LaravelPackageTools\PackageServiceProvider;
+use Illuminate\Support\ServiceProvider;
 
-class ClaudeServiceProvider extends PackageServiceProvider
+class ClaudeServiceProvider extends ServiceProvider
 {
-    public function configurePackage(Package $package): void
+    public function register(): void
     {
-        $package
-            ->name('laravel-claude')
-            ->hasConfigFile();
-    }
+        $this->mergeConfigFrom(__DIR__.'/../config/claude.php', 'claude');
 
-    public function registeringPackage(): void
-    {
         $this->app->bind(
             ClaudeClientContract::class,
             HttpClaudeClient::class
@@ -29,22 +23,10 @@ class ClaudeServiceProvider extends PackageServiceProvider
         });
     }
 
-    /**
-     * @note without Spatie
-     */
-    /*public function register(): void
-    {
-        $this->mergeConfigFrom(__DIR__.'/../config/feature-flags.php', 'feature-flags');
-
-        $this->app->singleton(FeatureFlagsManager::class, function ($app) {
-            return new FeatureFlagsManager(config('feature-flags'));
-        });
-    }
-
     public function boot(): void
     {
         $this->publishes([
-            __DIR__.'/../config/feature-flags.php' => config_path('feature-flags.php'),
-        ], 'feature-flags-config');
-    }*/
+            __DIR__.'/../config/claude.php' => config_path('claude.php'),
+        ], 'claude');
+    }
 }
