e894f6199a341bbda6610c3ba83342646a35c42e
Gaál Gergely <gaalgergely@yahoo.com>
2025-11-15 14:38:43 +0100

    Update FilesSchema.php

--- src/Validators/Files/FilesSchema.php
+++ src/Validators/Files/FilesSchema.php
@@ -8,8 +8,6 @@ final class FilesSchema
 {
     public static function rules(): array
     {
-        $roleEnum = implode(',', array_map(fn (Role $role) => $role->value, Role::cases()));
-
         return [
             'name' => ['required', 'string'],
             'content' => ['required', 'string'],
